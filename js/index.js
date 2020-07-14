56c2b9e78754907bc980899d52f2c958c9991c39
Moisés Escurra <moisesmagno@gmail.com>
2020-07-13 23:51:51 -0300

    Criado a versão mobile do site e também criado o menu responsivo.

---
+++ js/index.js
@@ -0,0 +1,9 @@
+function showMenuMobile() {
+    var contentMenu = document.querySelector(".content-menu-mobile");
+    contentMenu.style.display = "block";
+}
+
+function closeMenuMobile(){
+    var contentMenu = document.querySelector(".content-menu-mobile");
+    contentMenu.style.display = "none";
+}
